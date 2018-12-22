11666fa1c0399ba55527f9e18eb03bc1141be8c3
aguncn <aguncn@163.com>
2018-12-22 23:03:15 +0800

    实现基于函数的视图。
    Signed-off-by: aguncn <aguncn@163.com>

--- django_demo_project/urls.py
+++ django_demo_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bbs import views as bbs_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', bbs_view.index, name='index'),
 ]
